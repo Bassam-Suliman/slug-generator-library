dbfb430c17153811a13d1a6d4fe6395b65f81478
Bassam Sulaiman <biso.sul360@gmail.com>
2026-03-21 10:37:27 +0300

    added generateUnique and fixed bugs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { transliterate as tr } from 'transliteration';
 export function slugify(text: string, separator: string = '-'): string {
     // takes a string and converts it to a url-friendly slug
     // url-safe characters: a-z, A-Z, 0-9, and separator
+
+    if(typeof text !== 'string' || typeof separator !== 'string') {
+        throw new Error('Input must be a string');
+    }
+
     const alphanumericRegex = new RegExp('[^a-zA-Z0-9' + separator + ']', 'g');
     return tr(text)
         .toLowerCase()
@@ -10,4 +15,15 @@ export function slugify(text: string, separator: string = '-'): string {
         .trim()
         .replace(/[ ]+/g, separator)
         .replace(alphanumericRegex, '')
+};
+
+export function generateUnique(text: string, separator: string = '-'): string {
+    // takes a string and generates a unique slug by appending a random string
+    
+    if(typeof text !== 'string' || typeof separator !== 'string') {
+        throw new Error('Input must be a string');
+    }
+    
+    const randomString = Math.random().toString(36).slice(2);
+    return text + separator + randomString;
 }
